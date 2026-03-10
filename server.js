98ec7cbe783406fc3f1651abebf40d4b7242031c
githubscientist <sathish.datascientist@gmail.com>
2026-03-10 19:17:19 +0530

    response as html

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const http = require('http');
 // create a http server
 const server = http.createServer((request, response) => {
     response.writeHead(200, {
-        'Content-Type': 'text/plain'
+        'Content-Type': 'text/html'
     });
-    response.end("hello nodejs");
+    response.end(`
+        <!doctype html>
+        <html>
+            <head>
+                <title>Test NodeJS API Server</title>
+            </head>
+            <body>
+                <h1>Hello, World!</h1>
+            </body>
+        </html>
+        `);
 });
 
 // start the http server
