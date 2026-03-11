cbd719034ef798476098129e81e2f1187e557000
githubscientist <sathish.datascientist@gmail.com>
2026-03-11 21:02:58 +0530

    mongoose --setup done + dotenv --setup done

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const app = require("./app");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-// start the express application
-app.listen(3001, (error) => {
-    if (error) {
-        console.log('error starting the server:', error.message);
-        return;
-    }
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('connected to mongodb database...');
 
-    console.log('server listening at http://localhost:3001');
-});
+        // start the express application
+        app.listen(3001, (error) => {
+            if (error) {
+                console.log('error starting the server:', error.message);
+                return;
+            }
+
+            console.log('server listening at http://localhost:3001');
+        });
+    })
+    .catch((error) => {
+        console.log('error connecting to mongodb database...', error.message);
+    })
